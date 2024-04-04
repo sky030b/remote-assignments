5f568c4e01c413cea762c9e9bc24df69e1e0c99f
sky030b <sky030blue030@gmail.com>
2024-04-04 21:41:40 +0800

    完成作業四的要求二

--- Week-2/Assignment-4/index.js
+++ Week-2/Assignment-4/index.js
@@ -2,3 +2,9 @@ const welcomeBanner = document.querySelector(".welcome-banner");
 welcomeBanner.addEventListener("click", () => {
     welcomeBanner.querySelector("h3").innerText = "Have a Good Time!";
 })
+
+const actionBtn = document.querySelector(".actionBtn");
+actionBtn.addEventListener("click", () => {
+    const hiddenBoxes = document.querySelector(".content-boxes.d-none");
+    hiddenBoxes ? hiddenBoxes.classList.remove("d-none") : null;
+})
